73f79390ae5787e1af80e76c0637fdf064d029b2
Joseph Sawyer <joseph@jcsawyer.com>
2024-12-28 13:28:51 +0000

    feat: add importing view

---
+++ Jc.MediaImporter/Views/Import/ImportingView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Jc.MediaImporter.Views.Import;
+
+public partial class ImportingView : UserControl
+{
+    public ImportingView()
+    {
+        InitializeComponent();
+    }
+}
