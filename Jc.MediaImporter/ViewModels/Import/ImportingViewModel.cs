f7f922676b79516c7b25e54b127dfe86ac168629
Joseph Sawyer <joseph@jcsawyer.com>
2024-12-28 15:04:37 +0000

    feat: wire up importingviewmodel in process

--- Jc.MediaImporter/ViewModels/Import/ImportingViewModel.cs
+++ Jc.MediaImporter/ViewModels/Import/ImportingViewModel.cs
@@ -1,6 +1,22 @@
+using System;
+using System.Collections.Generic;
+
 namespace Jc.MediaImporter.ViewModels.Import;
 
 public class ImportingViewModel : ViewModelBase
 {
+    private readonly ImportViewModel _import;
+
+    private readonly IReadOnlyList<MediaFileViewModel> _photos;
+    public IReadOnlyList<MediaFileViewModel> Photos => _photos;
     
+    private readonly IReadOnlyList<MediaFileViewModel> _videos;
+    public IReadOnlyList<MediaFileViewModel> Videos => _videos;
+    
+    public ImportingViewModel(ImportViewModel import, List<MediaFileViewModel> photos, List<MediaFileViewModel> videos)
+    {
+        _import = import ?? throw new ArgumentNullException(nameof(import));
+        _photos = photos;
+        _videos = videos;
+    }
 }
